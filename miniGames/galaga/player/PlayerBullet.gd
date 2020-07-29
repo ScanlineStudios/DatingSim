529e1252c9c7a9a4d3ae07f082e1c422e73f9641
rosscwagner <rosscwagner@gmial.com>
2020-07-28 20:58:55 -0400

    Moved galaga stuff to minigames folder. Made enemy and player physics based

---
+++ miniGames/galaga/player/PlayerBullet.gd
@@ -0,0 +1,12 @@
+extends "res://miniGames/actor.gd"
+
+var explosion = preload("res://miniGames/galaga/Explosion0.tscn")
+
+
+func _on_PlayerBullet_body_entered(body):
+	# if !body.is_in_group("player"):
+	var explosion_instance = explosion.instance()
+	explosion_instance.position = get_global_position()
+	get_tree().get_root().add_child(explosion_instance)
+	queue_free()
+	
--- miniGames/galaga/player/galagaShip.gd
+++ miniGames/galaga/player/galagaShip.gd
@@ -1,23 +1,14 @@
-extends KinematicBody2D
+extends "res://miniGames/actor.gd"
 
-
-#Dircetion constants
-const LEFT = Vector2(-1,0)
-const RIGHT = Vector2(1,0)
-const UP = Vector2(0,-1)
-const DOWN = Vector2(0,1)
-const FORWARD = UP
-
-export var speed = 100 
+# export var speed = 100 
 export var bullet_speed = 100
 # lazers/sec
 export var fire_rate = 0.2
 
-var bullet = preload("res://galaga/player/PlayerBullet.tscn")
+var bullet = preload("res://miniGames/galaga/player/PlayerBullet.tscn")
 var can_fire = true
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
 	
 	if Input.is_action_pressed("fire") and can_fire:
 		var bullet_instance = bullet.instance()
@@ -30,6 +21,7 @@ func _process(delta):
 		can_fire = true
 		
 func _physics_process(delta):
+
 	var direction = Vector2()
 	if Input.is_action_pressed("move_up"):
 		direction += UP
@@ -40,9 +32,10 @@ func _physics_process(delta):
 	if Input.is_action_pressed("move_right"):
 		direction += RIGHT
 	
-	move_and_collide(direction * speed * delta)
+	apply_central_impulse(direction * move_speed * delta)
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	# put player ship in initial starting position 
-	pass # Replace with function body.
+	# lock rotation 
+	set_mode(MODE_CHARACTER) 
+	
 
