b16839974af09c09cbd1e83f789d33154da7c825
rosscwagner <rosscwagner@gmial.com>
2022-02-25 12:16:01 -0500

    Placeholder art and progress on dialog to mini game transition

--- scripts/ControlActive.gd
+++ scripts/ControlActive.gd
@@ -1,13 +1,30 @@
-extends Control
+extends Node2D
 
 # This script is used to control wheather a scene is active or not. For example when a mini game 
 # overlay come up during dialog the diolag scene will be set to inactive wile the plauer plays the 
 # mini game then set back to active when the player is done   
 
 var is_active = true setget set_is_active
+#var timer = get_tree().create_timer(10.0)
+onready var freeze = get_parent().get_node("Freeze")
+onready var dialog = get_parent().get_node("DialogSkeleton")
+onready var game = get_child(0)
 
-func _unhandled_input(event):
-	pass
+
+func _ready():
+	game.visible = false
+	game.set_process(false)
+	game.hide()
+	SignalManager.connect("start_tamerin_minigame", self, "_on_start_tamerin_minigame")
+	#freeze.freeze_scene(game, true)
+	#freeze.freeze_scene(dialog, false)
+	#yield( get_tree().create_timer(2.0), "timeout")
+	#print("timer finished")
+	#freeze.freeze_scene(dialog, true)
+	
+
+#func _unhandled_input(event):
+#	pass
 	# TODO: set a buttion for toggling is_active for debugging?
 	#if event.is_action_pressed("paused"):
 	#	self.is_paused = !is_paused
@@ -15,8 +32,20 @@ func _unhandled_input(event):
 
 func set_is_active(value):
 	is_active = value
-	 = !is_active
+	# = !is_active
 	print(str(name.replace("@", "").replace(str(int(name)), "")) + "Active: " + str(is_active))
 	# TODO: impliment a visual indicator that corrosponds to is_active status
 	# visible = is_active
 
+
+
+func _on_start_tamerin_minigame():
+	print("minigame signal recieved")
+	
+	game.show()
+	game.set_process(true)
+	
+	#dialog.paused = true
+	freeze.freeze_scene(dialog, true)
+	dialog.hide()
+	freeze.freeze_scene(game, false)
