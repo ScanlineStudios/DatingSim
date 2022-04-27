bb32f879f7dcaa2b0b1038b3e29e44bdd02a32e2
rosscwagner <20172491+rosscwagner@users.noreply.github.com>
2022-04-27 12:40:25 -0400

    Start of minigame timer functionality

--- scripts/minigames/tamerinMinigame/tamerinMinigameManager.gd
+++ scripts/minigames/tamerinMinigame/tamerinMinigameManager.gd
@@ -6,9 +6,71 @@ onready var squad = preload("res://scenes/minigames/tamerinMinigame/enemies/Enem
 #other enemies here
 
 onready var score_label: Label = get_node("GUI/MarginContainer/HBoxContainer/VBoxContainer/Score") 
-onready var countdown_timer_label : Label = get_node("GUI/MarginContainer3/HBoxContainer2/VBoxContainer/CenterLabel")
+onready var countdown_timer_label: Label = get_node("GUI/MarginContainer3/HBoxContainer2/VBoxContainer/CenterLabel")
 onready var countdown_tick_timer = Timer.new()
+onready var game_timer_label: Label = get_node("GUI/MarginContainer2/HBoxContainer2/VBoxContainer/GameTimer")
 
+var started: bool = false
+var time_passed: float = 0.0
+var time_remaining
+var duration_seconds: int
+
+
+var SECONDS_IN_MIN = 60
+var MINUTES_IN_HOUR = 60
+
+# called after ending triger (player death, time out, all enemies dead)
+func end():
+	
+	started = false
+	
+	update_center_label("GAME OVER")
+	
+	var score = score_label.score
+	SignalManager.emit_signal("tamerin_minigame_ended", score)
+	
+
+func _on_tamerin_minigame_player_destroyed():
+	end()
+
+func _process(delta):
+	
+	# TODO: game timer logic here
+	if started:
+		time_passed += delta
+		time_remaining = duration_seconds - time_passed
+	
+		var game_timer_seconds = int(fmod(time_remaining, SECONDS_IN_MIN))
+		var game_timer_minuites = int(fmod(time_remaining, SECONDS_IN_MIN * MINUTES_IN_HOUR) / MINUTES_IN_HOUR)
+		
+		update_game_timer_label(String(game_timer_minuites)+":"+String(game_timer_seconds))
+	
+
+# begine countdown then start spawning and rest of minigame. 
+# TODO: potential dificulty input, timer input?
+func start(_duration_seconds: int = 100, dificulty: int = 50) -> void:
+	
+	duration_seconds = _duration_seconds
+	# Countdown timer
+	# TODO reduce amount of magic numbers
+	# var seconds_to_countdown : int = 4 
+	for num in range(3,-1, -1):
+		countdown_tick_timer.start(1)
+		var label_text : String = String(num)
+		if (num == 0):
+			label_text = "GO!"
+		yield(countdown_tick_timer, "timeout")
+		update_center_label(label_text)
+		
+	started = true
+	
+	spawner.spawn_squads(squad, 7, 3)
+	
+	# clear center label
+	countdown_tick_timer.start(2)
+	yield(countdown_tick_timer, "timeout")
+	update_center_label("")
+	
 
 # Initialize tamerinMinigame but do not start spawning
 func _ready():
@@ -22,42 +84,12 @@ func _ready():
 	if (get_tree().get_root() == self.get_parent()):
 		start() 
 
-# called after ending triger (player death, time out, all enemies dead)
-func end():
-	update_center_label("GAME OVER")
-	
-	var score = score_label.score
-	SignalManager.emit_signal("tamerin_minigame_ended", score)
-	pass
 
-# begine countdown then start spawning and rest of minigame. 
-# TODO: potential dificulty input, timer input?
-func start(duration_seconds: int = 100, dificulty: int = 50) -> void:
-	
-	# Countdown timer
-	var seconds_to_countdown : int = 4 
-	for num in range(3,-1, -1):
-		countdown_tick_timer.start(1)
-		var label_text : String = String(num)
-		if (num == 0):
-			label_text = "GO!"
-		yield(countdown_tick_timer, "timeout")
-		update_center_label(label_text)
-		
-	spawner.spawn_squads(squad, 7, 3)
-	
-	# clear center label
-	countdown_tick_timer.start(2)
-	yield(countdown_tick_timer, "timeout")
-	update_center_label("")
-	
-
-func update_center_label(update_string : String = "") -> void:
+func update_center_label(update_string: String = "") -> void:
 	countdown_timer_label.text = update_string
 
-#func _on_countdown_tick_timer_timeout(update_string : String = ""):
-#   update_center_label(update_string)
+
+func update_game_timer_label(update_string: String = "") -> void:
+	game_timer_label.text = update_string
 
 
-func _on_tamerin_minigame_player_destroyed():
-	end()
