8e19a5ca97ce1de416522ab8d96bfe7ab222cb3c
rosscwagner <20172491+rosscwagner@users.noreply.github.com>
2022-05-18 15:06:11 -0400

    Working character info load/save prototype

--- scripts/enterCharacterInfo.gd
+++ scripts/enterCharacterInfo.gd
@@ -6,6 +6,10 @@ onready var preview_base_text: String = preview_label.text
 
 onready var suit_color_rect: ColorRect = get_node("MarginContainer2/VBoxContainer/HBoxContainer2/MarginContainer/ColorRect") 
 
+onready var save_button: Button = get_node("MarginContainer3/Button")
+
+onready var NEXT_SCENE: String = "res://scenes/testScenes/characterInfoLoadTest.tscn"
+
 var character_info: Dictionary = {
 	"character_name": "",
 	"pronoun_subject": "",
@@ -13,6 +17,16 @@ var character_info: Dictionary = {
 	"pronoun_possessive": "",
 	"suit_color": "" 
 }
+
+
+func check_info_filled()-> void:
+	var all_filled: bool = true
+	for k in character_info:
+		if typeof(character_info[k]) == TYPE_STRING && character_info[k] == "":
+			all_filled = false
+	save_button.disabled = !all_filled
+	print_debug(all_filled)
+
 
 func get_new_label_text()->String:
 	var label_text = preview_base_text 
@@ -38,6 +52,9 @@ func _ready():
 
 func update_preview_label(text: String):
 	preview_label.text = text
+	
+	# check all character info filled in so save buttion can be unlocked
+	check_info_filled()
 
 
 func _on_LineEditName_text_changed(new_text):
@@ -72,3 +89,12 @@ func _on_ColorPickerSuit_color_changed(color):
 	character_info.suit_color = color
 	
 	suit_color_rect.color = character_info.suit_color
+	check_info_filled()
+
+
+func _on_Button_pressed():
+	# Save character info to file
+	var file = File.new()
+	file.open("gameData/character_info.json", File.WRITE) 
+	file.store_string((to_json(character_info)))
+	get_tree().change_scene(NEXT_SCENE)
