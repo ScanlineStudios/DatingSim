9b86f0a5c1c64b90401c54a282e1868932b93f50
Radix <20172491+radix-r@users.noreply.github.com>
2022-06-19 19:34:00 -0400

    Changed tabs to spaces. Start of slot machine external project integration

--- scripts/characterInfoLoadTest.gd
+++ scripts/characterInfoLoadTest.gd
@@ -5,11 +5,11 @@ var FILE_NAME: String = Utility.save_slot_location+"/character_info.json"
 
 
 func _ready():
-	print_debug(FILE_NAME)
-	var data: Dictionary = Utility.load_json(FILE_NAME)
-	var label_text: String = ""
-	
-	for k in data:
-		label_text+= k+": "+data[k]+"\n"
-		
-	load_test_label.text = label_text
+    print_debug(FILE_NAME)
+    var data: Dictionary = Utility.load_json(FILE_NAME)
+    var label_text: String = ""
+    
+    for k in data:
+        label_text+= k+": "+data[k]+"\n"
+        
+    load_test_label.text = label_text
