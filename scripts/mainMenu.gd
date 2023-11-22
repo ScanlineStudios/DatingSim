53be44f034d2c344c0609184d3b57c93e05531f2
radix-r <20172491+radix-r@users.noreply.github.com>
2023-11-22 12:42:38 -0500

    Character sprite test start

--- scripts/mainMenu.gd
+++ scripts/mainMenu.gd
@@ -5,9 +5,12 @@ export var save_slot_scene: PackedScene
 
 # TODO: Continue functionality
 
-func _on_buttonStart_pressed():
-    Utility.game_manager.set_active_node(test_scene.instance())
+
 
 
 func _on_buttonNew_pressed():
     Utility.game_manager.set_active_node(save_slot_scene.instance())
+
+
+func _on_buttonCharacterTest_pressed() -> void:
+    Utility.game_manager.start_dialogic("CharacterTests")
